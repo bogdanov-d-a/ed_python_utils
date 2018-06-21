6906ead452ea142ad4a77acadb5a2365f573d901
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-22 00:32:27 +0300

    Add info_2d to ed_task_deferrer

--- ed_task_deferrer.py
+++ ed_task_deferrer.py
@@ -16,3 +16,18 @@ def info(state, now_date=None):
                 out_file.write(name + '\n')
 
     webbrowser.open(out_filename)
+
+def info_2d(columns, state, now_date=None):
+    if now_date is None:
+        now_date_ = datetime.datetime.now().date()
+        now_date = (now_date_.year, now_date_.month, now_date_.day)
+
+    with codecs.open(out_filename, 'w') as out_file:
+        for name, dates in sorted(state, key=operator.itemgetter(1)):
+            if len(columns) != len(dates):
+                raise Exception("Incorrect dates count for " + name)
+            for date_index in range(len(columns)):
+                if dates[date_index] <= now_date:
+                    out_file.write(name + ' - ' + columns[date_index] + '\n')
+
+    webbrowser.open(out_filename)
