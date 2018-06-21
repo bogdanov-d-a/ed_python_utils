5a8374db4744f0ba49f9758b0a47e2ee09b0e3bf
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-22 00:37:30 +0300

    Handle empty date in info_2d

--- ed_task_deferrer.py
+++ ed_task_deferrer.py
@@ -27,7 +27,7 @@ def info_2d(columns, state, now_date=None):
             if len(columns) != len(dates):
                 raise Exception("Incorrect dates count for " + name)
             for date_index in range(len(columns)):
-                if dates[date_index] <= now_date:
+                if dates[date_index] is not None and dates[date_index] <= now_date:
                     out_file.write(name + ' - ' + columns[date_index] + '\n')
 
     webbrowser.open(out_filename)
