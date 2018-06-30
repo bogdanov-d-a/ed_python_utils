6bfddda3e914b611715142f17e8cab5b07e437bb
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-30 12:13:34 +0300

    Add ed_file_data_viewer

---
+++ ed_file_data_viewer.py
@@ -0,0 +1,21 @@
+import os
+import webbrowser
+import tempfile
+
+def show_and_delete_file(filename):
+    try:
+        if not os.path.isfile(filename):
+            raise Exception(filename + ' file doesn\'t exist')
+        webbrowser.open(filename)
+        input()
+    finally:
+        os.remove(filename)
+
+def show_data_using_file(writer):
+    fd, path = tempfile.mkstemp('.txt')
+    try:
+        with open(path, 'w') as f:
+            writer(f)
+    finally:
+        os.close(fd)
+        show_and_delete_file(path)
