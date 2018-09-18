d523af328579b4d8bdade47a91656d54631105ab
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-09-18 23:38:05 +0300

    Extract info_writer function in ed_task_deferrer

--- ed_task_deferrer.py
+++ ed_task_deferrer.py
@@ -2,28 +2,30 @@ import datetime
 import operator
 from ed_file_data_viewer import show_data_using_file_simple
 
+
 def none_min(a, b):
     return b if a is None else min(a, b)
 
+
+def info_writer(out_file, state, now_date=None):
+    if now_date is None:
+        now_date_ = datetime.datetime.now().date()
+        now_date = (now_date_.year, now_date_.month, now_date_.day)
+
+    earliest_date = None
+
+    for name, date_ in sorted(state, key=operator.itemgetter(1)):
+        if date_ <= now_date:
+            out_file.write(name + '\n')
+        else:
+            earliest_date = none_min(earliest_date, date_)
+
+    out_file.write('Earliest date: ' + str(earliest_date) + '\n')
+
+
 def info(state, now_date=None):
-    def writer(out_file):
-        nonlocal now_date
+    show_data_using_file_simple(lambda out_file: info_writer(out_file, state, now_date))
 
-        if now_date is None:
-            now_date_ = datetime.datetime.now().date()
-            now_date = (now_date_.year, now_date_.month, now_date_.day)
-
-        earliest_date = None
-
-        for name, date_ in sorted(state, key=operator.itemgetter(1)):
-            if date_ <= now_date:
-                out_file.write(name + '\n')
-            else:
-                earliest_date = none_min(earliest_date, date_)
-
-        out_file.write('Earliest date: ' + str(earliest_date) + '\n')
-
-    show_data_using_file_simple(writer)
 
 def info_2d(columns, state, now_date=None):
     def writer(out_file):
