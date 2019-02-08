06ad1707c4ac946d862b12747bc45d01cf8ad04a
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2019-02-08 17:09:49 +0300

    Update screen on F5 press

--- ed_query_window.py
+++ ed_query_window.py
@@ -17,6 +17,8 @@ def run(data_provider):
         out_text.delete(1.0, tkinter.END)
         out_text.insert(tkinter.END, data_provider())
 
+    root.bind('<F5>', lambda e: calc())
+
     calc_button = tkinter.Button(root, command=calc)
     out_text = tkinter.Text(root, height=40, width=120)
     out_text_sb = tkinter.Scrollbar(root)
