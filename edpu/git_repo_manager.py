5913225bac7bbd019bce11c4bc128ae2ffc0f651
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-03-06 20:16:02 +0300

    Use data_provider for git_repo_manager

--- edpu/git_repo_manager.py
+++ edpu/git_repo_manager.py
@@ -1,7 +1,6 @@
 import argparse
 import codecs
 import os
-import edpu_user.git_repo_data
 from edpu import host_alias
 from edpu import git_tools
 from edpu import user_interaction
@@ -22,17 +21,17 @@ class Data:
         self.bundles = bundles
 
 
-def get_host_repos(filter_repos):
+def get_host_repos(repos, filter_repos):
     host = host_alias.get()
     result = {}
 
-    repos = edpu_user.git_repo_data.get().items()
+    repos_items = repos.items()
     if filter_repos is not None:
-        repos = filter(lambda elem: elem[0] in filter_repos, repos)
+        repos_items = filter(lambda elem: elem[0] in filter_repos, repos_items)
 
     all_storage = find_all_storage()
 
-    for repo_alias, repo in repos:
+    for repo_alias, repo in repos_items:
         path = repo.host_to_path.get(host)
 
         storage_remotes = {}
@@ -48,8 +47,8 @@ def get_host_repos(filter_repos):
     return result
 
 
-def host_repos_run(command, filter_repos, show_annotations=True):
-    for repo_alias, repo in get_host_repos(filter_repos).items():
+def host_repos_run(command, repos, filter_repos, show_annotations=True):
+    for repo_alias, repo in get_host_repos(repos, filter_repos).items():
         if show_annotations:
             print(repo_alias)
         command(repo_alias, repo)
@@ -58,12 +57,11 @@ def host_repos_run(command, filter_repos, show_annotations=True):
             print()
 
 
-def host_repos_run_with_path(command, filter_repos):
-    host_repos_run(lambda _, repo: command(repo.path), filter_repos)
+def host_repos_run_with_path(command, repos, filter_repos):
+    host_repos_run(lambda _, repo: command(repo.path), repos, filter_repos)
 
 
-def run_with_bundle_path(f):
-    bundle_path = edpu_user.git_repo_data.get_bundle_path()
+def run_with_bundle_path(bundle_path, f):
     if os.path.exists(bundle_path):
         raise Exception(bundle_path + ' exists')
     os.mkdir(bundle_path)
@@ -77,22 +75,22 @@ def run_with_bundle_path(f):
             pass
 
 
-def host_repos_status(filter_repos):
-    host_repos_run_with_path(git_tools.status, filter_repos)
+def host_repos_status(repos, filter_repos):
+    host_repos_run_with_path(git_tools.status, repos, filter_repos)
 
-def host_repos_fetch(filter_repos):
-    host_repos_run_with_path(git_tools.fetch, filter_repos)
+def host_repos_fetch(repos, filter_repos):
+    host_repos_run_with_path(git_tools.fetch, repos, filter_repos)
 
-def host_repos_all_refs(filter_repos):
-    host_repos_run_with_path(git_tools.all_refs, filter_repos)
+def host_repos_all_refs(repos, filter_repos):
+    host_repos_run_with_path(git_tools.all_refs, repos, filter_repos)
 
-def host_repos_all_stash(filter_repos):
-    host_repos_run_with_path(git_tools.all_stash, filter_repos)
+def host_repos_all_stash(repos, filter_repos):
+    host_repos_run_with_path(git_tools.all_stash, repos, filter_repos)
 
-def host_repos_all_create_bundle(filter_repos):
+def host_repos_all_create_bundle(bundle_hash_path_provider, bundle_path, repos, filter_repos):
     def bundle_path_callback(bundle_path):
         target_aliases = set([])
-        for _, repo in get_host_repos(filter_repos).items():
+        for _, repo in get_host_repos(repos, filter_repos).items():
             for target_alias in repo.bundles:
                 target_aliases.add(target_alias)
         target_aliases = sorted(target_aliases)
@@ -112,7 +110,7 @@ def host_repos_all_create_bundle(filter_repos):
                     f.write(line)
 
             if target_alias in repo.bundles:
-                hash_file_path = edpu_user.git_repo_data.get_bundle_hash_path(target_alias, repo_alias)
+                hash_file_path = bundle_hash_path_provider(target_alias, repo_alias)
                 last_hash = load_line(hash_file_path)
                 last_hash_or_root = 'root' if last_hash is None else last_hash
                 now_hash = git_tools.rev_parse(repo.path, 'HEAD')
@@ -134,11 +132,11 @@ def host_repos_all_create_bundle(filter_repos):
             else:
                 print('No {0} bundle provided'.format(target_alias))
 
-        host_repos_run(create_bundle, filter_repos)
+        host_repos_run(create_bundle, repos, filter_repos)
 
-    run_with_bundle_path(bundle_path_callback)
+    run_with_bundle_path(bundle_path, bundle_path_callback)
 
-def host_repos_all_get_user_bundle_info(filter_repos):
+def host_repos_all_get_user_bundle_info(user_bundle_info_path, repos, filter_repos):
     result = []
 
     def get_user_bundle_info(repo_alias, repo):
@@ -146,12 +144,12 @@ def host_repos_all_get_user_bundle_info(filter_repos):
         now_hash = git_tools.rev_parse(repo.path, 'HEAD')
         result.append(now_hash + ' ' + repo_alias)
 
-    host_repos_run(get_user_bundle_info, filter_repos, False)
+    host_repos_run(get_user_bundle_info, repos, filter_repos, False)
 
-    with codecs.open(edpu_user.git_repo_data.get_user_bundle_info_path(), 'w', 'utf-8') as f:
+    with codecs.open(user_bundle_info_path, 'w', 'utf-8') as f:
         f.write('|'.join(result))
 
-def host_repos_all_create_user_bundle(filter_repos):
+def host_repos_all_create_user_bundle(user_bundle_info_new_path, bundle_path, repos, filter_repos):
     def bundle_path_callback(bundle_path):
         user_info = {}
         for line in input().split('|'):
@@ -176,16 +174,15 @@ def host_repos_all_create_user_bundle(filter_repos):
             else:
                 print('No {0} bundle requested'.format(repo_alias))
 
-        host_repos_run(create_user_bundle, filter_repos)
+        host_repos_run(create_user_bundle, repos, filter_repos)
 
         if user_info != user_info_new:
-            with codecs.open(edpu_user.git_repo_data.get_user_bundle_info_new_path(), 'w', 'utf-8') as f:
+            with codecs.open(user_bundle_info_new_path, 'w', 'utf-8') as f:
                 f.write('|'.join(map(lambda e: e[1] + ' ' + e[0], user_info_new.items())))
 
-    run_with_bundle_path(bundle_path_callback)
+    run_with_bundle_path(bundle_path, bundle_path_callback)
 
-def host_repos_all_apply_user_bundle(filter_repos):
-    bundle_path = edpu_user.git_repo_data.get_bundle_path()
+def host_repos_all_apply_user_bundle(bundle_path, repos, filter_repos):
     if not os.path.exists(bundle_path):
         raise Exception(bundle_path + ' doesn\'t exist')
 
@@ -199,53 +196,53 @@ def host_repos_all_apply_user_bundle(filter_repos):
         else:
             print('No {0} bundle provided'.format(repo_alias))
 
-    host_repos_run(apply_user_bundle, filter_repos)
+    host_repos_run(apply_user_bundle, repos, filter_repos)
 
-def host_repos_fsck(filter_repos):
-    host_repos_run_with_path(git_tools.fsck, filter_repos)
+def host_repos_fsck(repos, filter_repos):
+    host_repos_run_with_path(git_tools.fsck, repos, filter_repos)
 
-def host_repos_gc(filter_repos):
-    host_repos_run_with_path(git_tools.gc, filter_repos)
+def host_repos_gc(repos, filter_repos):
+    host_repos_run_with_path(git_tools.gc, repos, filter_repos)
 
 def handle_all_storage(repo, handler):
     for alias, storage_path in repo.remotes.storage.items():
         print('Processing ' + alias)
         handler(storage_path)
 
-def host_repos_fetch_storage(filter_repos):
+def host_repos_fetch_storage(repos, filter_repos):
     def fetch_all_storage(_, repo):
         handle_all_storage(repo, lambda path: git_tools.fetch_remote(repo.path, path))
-    host_repos_run(fetch_all_storage, filter_repos)
+    host_repos_run(fetch_all_storage, repos, filter_repos)
 
-def host_repos_pull_storage(filter_repos):
+def host_repos_pull_storage(repos, filter_repos):
     def pull_all_storage(_, repo):
         handle_all_storage(repo, lambda path: git_tools.pull_with_checkout_multi(repo.path, path, repo.branches))
-    host_repos_run(pull_all_storage, filter_repos)
+    host_repos_run(pull_all_storage, repos, filter_repos)
 
-def host_repos_push_storage(filter_repos):
+def host_repos_push_storage(repos, filter_repos):
     def push_all_storage(_, repo):
         handle_all_storage(repo, lambda path: git_tools.push_all(repo.path, path))
-    host_repos_run(push_all_storage, filter_repos)
+    host_repos_run(push_all_storage, repos, filter_repos)
 
-def host_repos_fsck_storage(filter_repos):
+def host_repos_fsck_storage(repos, filter_repos):
     def fsck_all_storage(_, repo):
         handle_all_storage(repo, lambda path: git_tools.fsck(path))
-    host_repos_run(fsck_all_storage, filter_repos)
+    host_repos_run(fsck_all_storage, repos, filter_repos)
 
-def host_repos_pull_native(filter_repos):
+def host_repos_pull_native(repos, filter_repos):
     def pull_repo_native(_, repo):
         for remote in repo.remotes.native:
             git_tools.fetch_merge_with_checkout_multi(repo.path, remote, repo.branches)
-    host_repos_run(pull_repo_native, filter_repos)
+    host_repos_run(pull_repo_native, repos, filter_repos)
 
-def host_repos_push_native(filter_repos):
+def host_repos_push_native(repos, filter_repos):
     def push_repo_native(_, repo):
         for remote in repo.remotes.native:
             git_tools.push_multi(repo.path, remote, repo.branches)
-    host_repos_run(push_repo_native, filter_repos)
+    host_repos_run(push_repo_native, repos, filter_repos)
 
 
-def main():
+def main(data_provider):
     parser = argparse.ArgumentParser()
     parser.add_argument('--action')
     parser.add_argument('--bootstrap', action='store_true')
@@ -256,42 +253,42 @@ def main():
         print('bootstrap == ' + str(args.bootstrap))
         print()
 
-        bootstrap_mode_filter = lambda: edpu_user.git_repo_data.bootstrap_repos() if args.bootstrap else None
+        bootstrap_mode_filter = lambda: data_provider.get_bootstrap_repos() if args.bootstrap else None
         if args.action == 'status_all':
-            host_repos_status(bootstrap_mode_filter())
+            host_repos_status(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'ref_status_all':
-            host_repos_all_refs(bootstrap_mode_filter())
+            host_repos_all_refs(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'fetch_all':
-            host_repos_fetch(bootstrap_mode_filter())
+            host_repos_fetch(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'fetch_storage_all':
-            host_repos_fetch_storage(bootstrap_mode_filter())
+            host_repos_fetch_storage(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'pull_storage_all':
-            host_repos_pull_storage(bootstrap_mode_filter())
+            host_repos_pull_storage(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'push_storage_all':
-            host_repos_push_storage(bootstrap_mode_filter())
+            host_repos_push_storage(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'run_fsck_storage_all':
-            host_repos_fsck_storage(bootstrap_mode_filter())
+            host_repos_fsck_storage(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'run_fsck_all':
-            host_repos_fsck(bootstrap_mode_filter())
+            host_repos_fsck(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'gc_all':
-            host_repos_gc(bootstrap_mode_filter())
+            host_repos_gc(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'stash_all':
-            host_repos_all_stash(bootstrap_mode_filter())
+            host_repos_all_stash(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'create_bundle_all':
-            host_repos_all_create_bundle(bootstrap_mode_filter())
+            host_repos_all_create_bundle(lambda target_alias, repo_alias: data_provider.get_bundle_hash_path(target_alias, repo_alias), data_provider.get_bundle_path(), data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'get_user_bundle_info':
-            host_repos_all_get_user_bundle_info(bootstrap_mode_filter())
+            host_repos_all_get_user_bundle_info(data_provider.get_user_bundle_info_path(), data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'create_user_bundle':
-            host_repos_all_create_user_bundle(bootstrap_mode_filter())
+            host_repos_all_create_user_bundle(data_provider.get_user_bundle_info_new_path(), data_provider.get_bundle_path(), data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'apply_user_bundle':
-            host_repos_all_apply_user_bundle(bootstrap_mode_filter())
+            host_repos_all_apply_user_bundle(data_provider.get_bundle_path(), data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'pull_native_all':
-            host_repos_pull_native(bootstrap_mode_filter())
+            host_repos_pull_native(data_provider.get_repos(), bootstrap_mode_filter())
         elif args.action == 'push_native_all':
-            filter_ = edpu_user.git_repo_data.autopush_repos()
+            filter_ = data_provider.get_autopush_repos()
             if args.bootstrap:
                 filter_ &= bootstrap_mode_filter()
-            host_repos_push_native(filter_)
+            host_repos_push_native(data_provider.get_repos(), filter_)
         else:
             raise Exception('unexpected action ' + args.action)
     else:
@@ -360,5 +357,5 @@ def main():
                 raise Exception('unexpected action')
 
 
-if __name__ == '__main__':
-    pause_at_end.run(main, 'Program finished successfully')
+def run(data_provider):
+    pause_at_end.run(lambda: main(data_provider), 'Program finished successfully')
