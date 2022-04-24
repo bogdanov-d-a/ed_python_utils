f4068e5342a14f9941843411c098a9f4b45060fc
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-04-24 23:01:51 +0300

    Add pick_option_multi to user_interaction

--- edpu/user_interaction.py
+++ edpu/user_interaction.py
@@ -24,3 +24,30 @@ def pick_option(prompt, options):
         raise Exception('Number is too high')
 
     return result - 1
+
+
+def pick_option_multi(prompt, options):
+    index = 1
+    for option in options:
+        print(str(index) + '. ' + option)
+        index += 1
+
+    print(prompt)
+    selection = set()
+
+    while True:
+        print(selection)
+        user_data = int(input())
+
+        if user_data == 0:
+            return selection
+
+        if user_data < 1:
+            print('Number is too low')
+        elif user_data > len(options):
+            print('Number is too high')
+        else:
+            if user_data in selection:
+                selection.remove(user_data)
+            else:
+                selection.add(user_data)
