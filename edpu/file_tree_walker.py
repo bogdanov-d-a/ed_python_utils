0c4147635ffdae62bb1fdaa3cb49d3b0d2b4b49d
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-02-03 23:54:47 +0600

    Add dir walking messages

--- edpu/file_tree_walker.py
+++ edpu/file_tree_walker.py
@@ -12,6 +12,9 @@ def walk(root_path: str, ignore_callback: Callable[[str, list[str]], bool]=lambd
 
     result: dict[str, list[list[str]]] = { TYPE_DIR: [], TYPE_FILE: [] }
 
+    print('Walking dir ' + root_path)
+    file_count = 0
+
     for cur_root_path, dirs, files in os.walk(root_path):
         rel_path_text = os.path.relpath(cur_root_path, root_path)
 
@@ -21,11 +24,18 @@ def walk(root_path: str, ignore_callback: Callable[[str, list[str]], bool]=lambd
             rel_path = rel_path_text.split(os.sep)
 
         def handler(elems: list[str], type: str) -> None:
+            nonlocal file_count
+
+            if type == TYPE_FILE:
+                file_count += len(elems)
+
             for elem in elems:
                 if not ignore_callback(type, rel_path + [elem]):
                     result[type].append(rel_path + [elem])
 
         handler(dirs, TYPE_DIR)
         handler(files, TYPE_FILE)
+
+    print('Walking complete, file count - ' + str(file_count))
 
     return result
