22712ea5c809da7f3737c2fc79f8a683091c5465
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-02-06 11:25:16 +0600

    Add pick_str_option_multi

--- edpu/user_interaction.py
+++ edpu/user_interaction.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 
 def list_to_dict(list_: list[tuple[str, Any]]) -> dict[str, Any]:
@@ -10,6 +10,15 @@ def list_to_dict(list_: list[tuple[str, Any]]) -> dict[str, Any]:
         result[key] = value
 
     return result
+
+
+def generate_cmds(list_: list[Any]) -> list[tuple[str, Any]]:
+    cmds = '1234567890qwertyuiopasdfghjklzxcvbnm'
+
+    if len(list_) > len(cmds):
+        raise Exception('list is too long')
+
+    return list(zip(cmds[:len(list_)], list_))
 
 
 def yes_no_prompt(msg: str) -> bool:
@@ -94,3 +103,42 @@ def pick_str_option_ex(prompt: str, options: list[tuple[str, str, Any]]) -> Any:
     pick: dict[str, str] = list_to_dict(list(map(lambda e: (e[0], e[1]), options)))
     result = list_to_dict(list(map(lambda e: (e[0], e[2]), options)))
     return result[pick_str_option(prompt, pick)]
+
+
+def pick_str_option_multi(prompt: str, options: list[tuple[str, str]]) -> list[str]:
+    option_cmds = set(map(lambda e: e[0], options))
+
+    for option_cmd in option_cmds:
+        if len(option_cmd) != 1:
+            raise Exception('incorrect option command ' + option_cmd + ' found')
+
+    def get_user_char_set() -> Optional[set[str]]:
+        user_str = input()
+
+        user_char_set: set[str] = set()
+
+        for user_char in user_str:
+            if user_char not in option_cmds:
+                print('unexpected char ' + user_char + ' found')
+                return None
+
+            if user_char in user_char_set:
+                print('duplicate char ' + user_char + ' found')
+                return None
+
+            user_char_set.add(user_char)
+
+        return user_char_set
+
+    while True:
+        for option_cmd, option_text in options:
+            print(option_cmd + ': ' + option_text)
+
+        print(prompt)
+
+        result = get_user_char_set()
+
+        if result is None:
+            continue
+
+        return list(result)
