2c541f845503e51cc44f0a274d97fad754c9ccec
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-02-16 23:41:13 +0600

    Add features to edpu\string_utils.py

--- edpu/string_utils.py
+++ edpu/string_utils.py
@@ -3,6 +3,7 @@ def removeprefix(str_: str, prefix: str) -> str:
         return str_[len(prefix):]
     return str_
 
+
 def removesuffix(str_: str, suffix: str) -> str:
     if len(suffix) > len(str_):
         return str_
@@ -10,3 +11,31 @@ def removesuffix(str_: str, suffix: str) -> str:
     if str_suffix_pos == len(str_) - len(suffix):
         return str_[:str_suffix_pos]
     return str_
+
+
+def char_wrap(str_: str, char: str) -> str:
+    return char + str_ + char
+
+
+def apostrophe_wrap(str_: str) -> str:
+    return char_wrap(str_, '\'')
+
+
+def quotation_mark_wrap(str_: str) -> str:
+    return char_wrap(str_, '"')
+
+
+def backtick_wrap(str_: str) -> str:
+    return char_wrap(str_, '`')
+
+
+def merge_with_space(list_: list[str]) -> str:
+    return ' '.join(list_)
+
+
+def round_brackets_wrap(str_: str) -> str:
+    return '(' + str_ + ')'
+
+
+def comma_separate(list_: list[str]) -> str:
+    return ','.join(list_)
