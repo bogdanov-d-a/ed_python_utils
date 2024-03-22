23dadb023977462565de5b323ba83a8b3c77fbb7
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-03-23 03:00:27 +0500

    Fix edpu\explorer_launcher.py

--- edpu/explorer_launcher.py
+++ edpu/explorer_launcher.py
@@ -1,8 +1,13 @@
 def open_dir_in_explorer(dir: str) -> None:
+    from .powershell import powershell_command
     from .string_utils import merge_with_space, quotation_mark_wrap
     from os import system
+    from os.path import isdir
 
-    system(merge_with_space([
-        'explorer',
+    if not isdir(dir):
+        raise Exception()
+
+    system(powershell_command(merge_with_space([
+        'Invoke-Item',
         quotation_mark_wrap(dir),
-    ]))
+    ])))
