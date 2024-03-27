fe3507cef08aa48872b219f6242d5e756322b0b6
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-03-27 22:24:28 +0500

    Update edpu\win_reg.py

--- edpu/win_reg.py
+++ edpu/win_reg.py
@@ -1,3 +1,6 @@
+from typing import Any, Iterable
+
+
 def reg_delete_gen(key: str) -> str:
     from .string_utils import merge_with_space, quotation_mark_wrap
 
@@ -12,3 +15,29 @@ def reg_delete_gen(key: str) -> str:
 def reg_delete(key: str) -> None:
     from os import system
     system(reg_delete_gen(key))
+
+
+def enum_key(key: Any) -> Iterable[str]:
+    index = 0
+
+    while True:
+        try:
+            from winreg import EnumKey
+            yield EnumKey(key, index)
+            index += 1
+
+        except EnvironmentError:
+            break
+
+
+def enum_value(key: Any) -> Iterable[tuple[str, Any, int]]:
+    index = 0
+
+    while True:
+        try:
+            from winreg import EnumValue
+            yield EnumValue(key, index)
+            index += 1
+
+        except EnvironmentError:
+            break
