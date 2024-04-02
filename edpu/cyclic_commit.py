c0fa861ac0a1f3f2f79c11711d2c18fece5adcc6
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-04-02 08:41:25 +0500

    Add gitk to edpu\cyclic_commit.py

--- edpu/cyclic_commit.py
+++ edpu/cyclic_commit.py
@@ -7,7 +7,7 @@ def cyclic_commit(path: str) -> None:
         while not stop:
             from .citool_launcher import open_citool
             from .explorer_launcher import open_dir_in_explorer
-            from .git_repo_manager.utils.git import run_git_command, status
+            from .git_repo_manager.utils.git import run_command, run_git_command, status
             from .user_interaction import pick_str_option_ex
 
             def quit() -> None:
@@ -21,7 +21,8 @@ def cyclic_commit(path: str) -> None:
                 ('s', 'status', lambda: status(path)),
                 ('a', 'add', lambda: run_git_command(path, ['add', '.'])),
                 ('c', 'commit', lambda: run_git_command(path, ['commit', '-m', '1'])),
-                ('l', 'clean', lambda: run_git_command(path, ['clean', '-idx'])),
+                ('l', 'gitk', lambda: run_command(path, ['gitk'])),
+                ('n', 'clean', lambda: run_git_command(path, ['clean', '-idx'])),
             ])()
 
     pause_at_end.run(main, pause_at_end.DEFAULT_MESSAGE)
