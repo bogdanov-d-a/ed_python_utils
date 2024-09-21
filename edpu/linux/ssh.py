b90a847c2f88a7cc853c9ba686d8df854322d27b
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-09-21 07:03:51 +0300

    Improve edpu\linux\ssh.py

--- edpu/linux/ssh.py
+++ edpu/linux/ssh.py
@@ -1,13 +1,22 @@
 SSH = 'ssh'
 
 
-def ssh_connect(host: str) -> str:
-    from ..string_utils import merge_with_space
-    from .root import root
-
-    return merge_with_space([
-        SSH,
+def ssh_user(user: str) -> list[str]:
+    return [
         '-l',
-        root(),
-        host,
-    ])
+        user,
+    ]
+
+
+def ssh_dont_store_keys() -> list[str]:
+    from ..string_utils import quotation_mark_wrap
+
+    return [
+        '-o',
+        quotation_mark_wrap('UserKnownHostsFile=/dev/null'),
+    ]
+
+
+def ssh(host: str, options: list[str]) -> str:
+    from ..string_utils import merge_with_space
+    return merge_with_space([SSH] + options + [host])
