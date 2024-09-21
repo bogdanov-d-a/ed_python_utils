534f87dc5e4c49b2de14f9750d7dd09d3d245c9d
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-09-21 06:59:24 +0300

    Improve batch_pipe_generate

--- edpu/batch.py
+++ edpu/batch.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 def batch_chcp_utf8() -> str:
     from .string_utils import merge_with_space
 
@@ -17,16 +20,24 @@ def batch_chcp_utf8_nul() -> str:
     ])
 
 
-def batch_pipe_generate(command: str, out1: str, out2: str, out: str) -> None:
+def batch_pipe_generate(command: str, out1: Optional[str], out2: Optional[str], out: str) -> None:
     from .string_utils import merge_with_space, quotation_mark_wrap
 
     with open(out, 'w', encoding='utf-8') as file:
         file.write(batch_chcp_utf8_nul() + '\n')
 
-        file.write(merge_with_space([
-            command,
-            '1>',
-            quotation_mark_wrap(out1),
-            '2>',
-            quotation_mark_wrap(out2),
-        ]) + '\n')
+        data = [command]
+
+        if out1 is not None:
+            data += [
+                '1>',
+                quotation_mark_wrap(out1),
+            ]
+
+        if out2 is not None:
+            data += [
+                '2>',
+                quotation_mark_wrap(out2),
+            ]
+
+        file.write(merge_with_space(data) + '\n')
