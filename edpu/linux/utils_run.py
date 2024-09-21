eed25e91dd527e5b1fd238c70c04eb17ede5482f
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-09-21 07:02:34 +0300

    Add drop_caches

--- edpu/linux/utils_run.py
+++ edpu/linux/utils_run.py
@@ -6,3 +6,8 @@ def lsblk() -> None:
 def sync() -> None:
     from os import system
     system(f'sync')
+
+
+def drop_caches() -> None:
+    from os import system
+    system(f'echo 3 | tee /proc/sys/vm/drop_caches')
