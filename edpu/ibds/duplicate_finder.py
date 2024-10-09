426e5bbfc6b2bde6a82eb8ffc3cb50af0d45e872
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2024-10-09 14:31:12 +0300

    Improve find_file_duplicates

--- edpu/ibds/duplicate_finder.py
+++ edpu/ibds/duplicate_finder.py
@@ -1,4 +1,6 @@
-from .user_data import CollectionDict, CollectionList, COLLECTION_VALUE_DUPLICATE_SKIP_PATHS
+from .. import storage_finder
+from ..string_utils import merge_with_space, quotation_mark_wrap
+from .user_data import COLLECTION_VALUE_LOCATIONS, CollectionDict, CollectionList, COLLECTION_VALUE_DUPLICATE_SKIP_PATHS
 from . import collection_definition
 from . import ibds_utils
 from . import file_tree_snapshot
@@ -6,7 +8,7 @@ from . import path_generator
 from . import storage_device
 
 
-def _collection_common(data_dir: str, collection_name: str, skip_paths: list[str]) -> None:
+def _collection_common(data_dir: str, collection_name: str, collection_path: str, skip_paths: list[str]) -> None:
     data = collection_definition.load_common_data(path_generator.gen_common_file_path(collection_name, data_dir))
     table: dict[str, list[str]] = {}
 
@@ -18,9 +20,23 @@ def _collection_common(data_dir: str, collection_name: str, skip_paths: list[str
         table[hash_].append(path)
 
     table_items: list[tuple[str, list[str]]] = ibds_utils.key_sorted_dict_items(table)
+
     for hash_, paths in table_items:
         if (len(paths) > 1):
-            print(hash_ + ' ' + str(paths))
+            print(f'REM {hash_}')
+
+            for path in paths:
+                from os.path import join
+
+                print(merge_with_space([
+                    'REM',
+                    'del',
+                    '/f',
+                    '/q',
+                    quotation_mark_wrap(join(collection_path, path)),
+                ]))
+
+            print()
 
 
 def _collection_storage_device(data_dir: str, collection_name: str, storage_device_: storage_device.StorageDevice) -> None:
@@ -38,7 +54,12 @@ def _collection_storage_device(data_dir: str, collection_name: str, storage_devi
             print(hash_ + ' ' + str(paths))
 
 
-def collections_common(data_dir: str, collection_dict: CollectionDict) -> None:
+def collections_common(data_dir: str, storage_device_: storage_device.StorageDevice, collection_dict: CollectionDict) -> None:
+    path_prefix = storage_finder.keep_getting_storage_path(storage_device_.getName()) if storage_device_.isRemovable() else ''
+
     collection_dict_items: CollectionList = ibds_utils.key_sorted_dict_items(collection_dict)
+
     for collection_name, data in collection_dict_items:
-        _collection_common(data_dir, collection_name, data[COLLECTION_VALUE_DUPLICATE_SKIP_PATHS])
+        for location in data[COLLECTION_VALUE_LOCATIONS]:
+            if location.getStorageDevice().getName() == storage_device_.getName():
+                _collection_common(data_dir, collection_name, path_prefix + location.getPath(), data[COLLECTION_VALUE_DUPLICATE_SKIP_PATHS])
--- edpu/ibds/main_app.py
+++ edpu/ibds/main_app.py
@@ -16,11 +16,15 @@ def run(user_data: user_data.UserData) -> None:
             storage_device_ = ibds_utils.pick_storage_device(user_data.getDeviceList())
             collection_scanner.scan_storage_device(user_data.getDataPath(), user_data.getCollectionDict(), storage_device_, user_data.getSkipMtime())
 
+        def find_file_duplicates() -> None:
+            storage_device_ = ibds_utils.pick_storage_device(user_data.getDeviceList())
+            duplicate_finder.collections_common(user_data.getDataPath(), storage_device_, user_data.getCollectionDict())
+
         actions: list[tuple[str, str, Callable[[], None]]] = [
             ('s', 'Scan location', scan_storage_device),
             ('c', 'Compare all data', lambda: collection_compare.collections(user_data.getDataPath(), user_data.getCollectionDict(), user_data.getCompareOnlyAvailable())),
             ('g', 'Generate collection definitions', lambda: collection_definition.generate_collections_definition(user_data.getDataPath(), user_data.getCollectionDict())),
-            ('d', 'Find file duplicates', lambda: duplicate_finder.collections_common(user_data.getDataPath(), user_data.getCollectionDict())),
+            ('d', 'Find file duplicates', find_file_duplicates),
             ('f', 'Check data file set', lambda: collection_file_set.check_data_file_set(user_data.getDataPath(), user_data.getCollectionDict())),
             ('u', 'Find unique data', lambda: collection_compare.collections_by_hash(user_data.getDataPath(), user_data.getCollectionDict())),
         ]
