4976c27dfd3262179be36a8f70961b3f71908f28
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-02-01 00:03:43 +0300

    Improve mpv_play

--- edpu/mpv.py
+++ edpu/mpv.py
@@ -20,18 +20,25 @@ def mpv_va_detect(path: str, rev: Optional[bool]=None) -> tuple[str, str]:
     return (contents[1], contents[0]) if rev_() else (contents[0], contents[1])
 
 
-def mpv_play(va: tuple[str, str], path: str) -> None:
-    from .popen_helper import popen_communicate_command_cwd
+def mpv_play(va: tuple[str, str], path: Optional[str]) -> None:
     from .string_utils import merge_with_space, quotation_mark_wrap
     from edpu_user.mpv import mpv_path
 
     v, a = va
 
-    popen_communicate_command_cwd(merge_with_space([
+    cmd = merge_with_space([
         quotation_mark_wrap(mpv_path()),
         quotation_mark_wrap(v),
         quotation_mark_wrap(f'--audio-file={a}'),
-    ]), path)
+    ])
+
+    if path is not None:
+        from .popen_helper import popen_communicate_command_cwd
+        popen_communicate_command_cwd(cmd, path)
+
+    else:
+        from .popen_helper import popen_communicate_command
+        popen_communicate_command(cmd)
 
 
 def mpv_va_detect_play(path: str, rev: Optional[bool]=None) -> None:
