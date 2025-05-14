f5fb274fbffac93e3a0c95a0a0bbbdfa2bc84f41
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-05-14 18:47:47 +0300

    Add wrap_with_print_line to edpu\cyclic_commit.py

--- edpu/cyclic_commit.py
+++ edpu/cyclic_commit.py
@@ -9,6 +9,11 @@ def cyclic_commit(path: str) -> None:
             from .explorer_launcher import open_dir_in_explorer
             from .git_repo_manager.utils.git import run_command, run_git_command, status, diff
             from .user_interaction import pick_str_option_ex
+            from typing import Callable
+
+            def wrap_with_print_line(fn: Callable[[], None]) -> Callable[[], None]:
+                from .print_line_wrapper import wrap_with_print_line as impl
+                return impl(fn, 3)
 
             def quit() -> None:
                 nonlocal stop
@@ -18,13 +23,13 @@ def cyclic_commit(path: str) -> None:
                 ('q', 'quit', quit),
                 ('e', 'explorer', lambda: open_dir_in_explorer(path)),
                 ('t', 'citool', lambda: open_citool(path)),
-                ('s', 'status', lambda: status(path)),
-                ('d', 'diff', lambda: diff(path)),
-                ('f', 'diff cached', lambda: diff(path, True)),
+                ('s', 'status', wrap_with_print_line(lambda: status(path))),
+                ('d', 'diff', wrap_with_print_line(lambda: diff(path))),
+                ('f', 'diff cached', wrap_with_print_line(lambda: diff(path, True))),
                 ('a', 'add', lambda: run_git_command(path, ['add', '.'])),
-                ('c', 'commit', lambda: run_git_command(path, ['commit', '-m', '1'])),
+                ('c', 'commit', wrap_with_print_line(lambda: run_git_command(path, ['commit', '-m', '1']))),
                 ('l', 'gitk', lambda: run_command(path, ['gitk'])),
-                ('n', 'clean', lambda: run_git_command(path, ['clean', '-idx'])),
+                ('n', 'clean', wrap_with_print_line(lambda: run_git_command(path, ['clean', '-idx']))),
             ])()
 
     pause_at_end.run(main, pause_at_end.DEFAULT_MESSAGE)
