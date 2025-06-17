a3e487a0f3e005e501e6835e14391581d7d16e00
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-06-17 16:37:22 +0300

    Use walk_type

--- edpu/ibds/utils/file_tree_scanner.py
+++ edpu/ibds/utils/file_tree_scanner.py
@@ -9,23 +9,25 @@ def _make_file_progress_printer(path_: str) -> Callable[[int], None]:
 
 def scan(root_path: str, skip_paths: list[str], use_descript_ion: bool, collector: Optional[time.Collector]=None) -> list[list[str]]:
     from ...context_manager import DummyContextManager
-    from ...file_tree_walker import walk, TYPE_DIR, TYPE_FILE
+    from ...file_tree_walker import walk_type, TYPE_FILE
     from .utils import path_needs_skip
 
     with time.get_perf_counter_measure(collector, time.Key.WALK) if collector is not None else DummyContextManager():
-        return walk(
+        return walk_type(
             root_path,
-            lambda type, path: type == TYPE_DIR or path_needs_skip(path, skip_paths, use_descript_ion),
+            TYPE_FILE,
+            lambda path: path_needs_skip(path, skip_paths, use_descript_ion),
             _make_file_progress_printer(root_path)
-        )[TYPE_FILE]
+        )
 
 
 def scan_descript_ion(root_path: str) -> list[list[str]]:
-    from ...file_tree_walker import walk, TYPE_DIR, TYPE_FILE
+    from ...file_tree_walker import walk_type, TYPE_FILE
     from ..utils.utils import DESCRIPT_ION
 
-    return walk(
+    return walk_type(
         root_path,
-        lambda type, path: type == TYPE_DIR or not (len(path) > 0 and path[-1] == DESCRIPT_ION),
+        TYPE_FILE,
+        lambda path: not (len(path) > 0 and path[-1] == DESCRIPT_ION),
         _make_file_progress_printer(root_path)
-    )[TYPE_FILE]
+    )
