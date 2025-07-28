79788fc1d02f9a51b20f48affb2eec77333641ff
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-07-28 20:11:44 +0300

    Refactor edpu\explorer_launcher.py

--- edpu/explorer_launcher.py
+++ edpu/explorer_launcher.py
@@ -1,13 +1,12 @@
 def open_dir_in_explorer(dir: str) -> None:
-    from .powershell import powershell_command
-    from .string_utils import merge_with_space, quotation_mark_wrap
-    from os import system
+    from .powershell import POWERSHELL_INVOKE_ITEM, escape_powershell_apostrophe, powershell_command
+    from .string_utils import apostrophe_wrap, merge_with_space
     from os.path import isdir
 
     if not isdir(dir):
         raise Exception()
 
-    system(powershell_command(merge_with_space([
-        'Invoke-Item',
-        quotation_mark_wrap(dir),
-    ])))
+    powershell_command(merge_with_space([
+        POWERSHELL_INVOKE_ITEM,
+        apostrophe_wrap(escape_powershell_apostrophe(dir)),
+    ]))
