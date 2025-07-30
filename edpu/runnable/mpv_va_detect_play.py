5a5c637794aba1abeb0fbe0758ca33be4c9bd9c9
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-07-30 21:07:45 +0300

    Improve edpu\runnable\mpv_va_detect_play.py

--- edpu/runnable/mpv_va_detect_play.py
+++ edpu/runnable/mpv_va_detect_play.py
@@ -1,3 +1,4 @@
 if __name__ == '__main__':
     from edpu.mpv import mpv_va_detect_play
-    mpv_va_detect_play(input())
+    from edpu.pause_at_end import run
+    run(lambda: mpv_va_detect_play(input()))
