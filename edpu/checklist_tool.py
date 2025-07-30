bd903ccb0cf143a21758d8a48d9d71305535664a
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-07-30 21:56:56 +0300

    Refactor edpu\checklist_tool.py

--- edpu/checklist_tool.py
+++ edpu/checklist_tool.py
@@ -1,72 +1,109 @@
 from __future__ import annotations
 from typing import Callable
-import tkinter
 
 
-Command = Callable[[], bool]
-
-
-class CbPack:
-    def __init__(self: CbPack, master: tkinter.Tk, text: str, row: int, command: Callable[[CbPack], None]) -> None:
-        self.var = tkinter.IntVar(master)
-
-        checkbutton = tkinter.Checkbutton(
-            master,
-            text=text,
-            variable=self.var,
-            command=lambda: command(self)
-        )
-
-        checkbutton.grid(row=row, sticky=tkinter.W)
+VoidFn = Callable[[], None]
+BoolFn = Callable[[bool], None]
 
 
 def show_checklist(items: list[str], title: str) -> None:
-    from . import tkinter_utils
+    from .tkinter_utils import non_resizable, center_window
+    from tkinter import Tk, StringVar
 
-    master = tkinter.Tk()
-    master.title(title)
 
-    info_text = tkinter.StringVar(master)
-    info = tkinter.Label(master, textvariable=info_text)
-    info.grid(row=0)
+    root = Tk()
+    root.title(title)
+    non_resizable(root)
 
     total_count = len(items)
     checked_count = 0
 
-    def update_info_text() -> None:
-        info_text.set('{0} stats: {1} / {2} ({3})'.format(
-            title, checked_count, total_count - checked_count, total_count))
 
-    update_info_text()
+    def add_label() -> StringVar:
+        variable = StringVar(root)
 
-    def cb_command_handler(cb_pack: CbPack) -> None:
-        nonlocal checked_count
-        if cb_pack.var.get() == 0:
-            checked_count -= 1
-        else:
-            checked_count += 1
-        update_info_text()
+        from tkinter.ttk import Label
+        Label(root, textvariable=variable).grid(row=0)
 
-    for item, row in zip(items, range(len(items))):
-        CbPack(master, item, row + 1, cb_command_handler)
+        return variable
 
-    tkinter_utils.center_window(master)
-    tkinter.mainloop()
+
+    def make_label_updater(variable: StringVar) -> VoidFn:
+        def impl() -> None:
+            variable.set(f'{title} : {checked_count} / {total_count - checked_count} ({total_count})')
+
+        impl()
+        return impl
+
+
+    def make_stat_updater(label_updater: VoidFn) -> BoolFn:
+        def impl(checked: bool) -> None:
+            nonlocal checked_count
+
+            if checked:
+                checked_count += 1
+            else:
+                checked_count -= 1
+
+            label_updater()
+
+        return impl
+
+
+    def add_checkbuttons(stat_updater: BoolFn) -> None:
+        for item, index in zip(items, range(total_count)):
+            def impl() -> None:
+                from tkinter import IntVar, W
+                from tkinter.ttk import Checkbutton
+
+                variable = IntVar(root)
+
+                Checkbutton(
+                    root,
+                    text=item,
+                    variable=variable,
+                    command=lambda: stat_updater(variable.get() != 0)
+                ).grid(row=index + 1, sticky=W)
+
+            impl()
+
+
+    add_checkbuttons(
+        make_stat_updater(
+            make_label_updater(
+                add_label()
+            )
+        )
+    )
+
+
+    center_window(root)
+    root.mainloop()
 
 
 def show_picker(checklists: list[tuple[str, list[str]]]) -> None:
-    from . import button_window
+    from .button_window import run, ButtonDefs
 
-    buttons: list[tuple[str, Command]] = []
 
-    def get_command(index: int) -> Command:
-        def command() -> bool:
-            show_checklist(checklists[index][1], checklists[index][0])
-            return False
+    def button_defs() -> ButtonDefs:
+        result: ButtonDefs = []
 
-        return command
+        for name, items in checklists:
+            from .button_window import ButtonCommand
 
-    for text, index in zip(list(map(lambda checklist: checklist[0], checklists)), range(len(checklists))):
-        buttons.append((text, get_command(index)))
+            def command() -> ButtonCommand:
+                name_copy = name
+                items_copy = items
 
-    button_window.run(buttons)
+                def impl() -> bool:
+                    show_checklist(items_copy, name_copy)
+                    return False
+
+                return impl
+
+            result.append((name, command()))
+
+        return result
+
+
+    run(button_defs(), 'Checklists')
