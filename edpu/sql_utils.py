3049fad965022232d94f222bf23d10d0aa07c6fd
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-08-02 01:08:09 +0300

    Extend edpu\sql_utils.py

--- edpu/sql_utils.py
+++ edpu/sql_utils.py
@@ -1,9 +1,13 @@
 from sqlalchemy import Connection, CursorResult, Row
-from typing import Any, Iterator, Sequence
+from typing import Any, Callable, Iterator, Sequence
 
 
+AND = 'AND'
+COUNT = 'COUNT'
 DISTINCT = 'DISTINCT'
 FROM = 'FROM'
+INSERT = 'INSERT'
+INTO = 'INTO'
 IS = 'IS'
 JOIN = 'JOIN'
 LEFT = 'LEFT'
@@ -12,14 +16,26 @@ NULL = 'NULL'
 ON = 'ON'
 OP_EQ = '='
 OP_NEQ = '<>'
+OR = 'OR'
 ORDER_BY = 'ORDER BY'
 SELECT = 'SELECT'
+VALUES = 'VALUES'
 WHERE = 'WHERE'
 
 
 def get_table_column_str(table: str, column: str) -> str:
     from .string_utils import backtick_wrap
     return backtick_wrap(table) + '.' + backtick_wrap(column)
+
+
+def round_brackets_enumeration(list_: list[str], conv: Callable[[str], str]) -> str:
+    from .string_utils import round_brackets_wrap, comma_separate
+
+    return round_brackets_wrap(
+        comma_separate(
+            list(map(conv, list_))
+        )
+    )
 
 
 def execute_sql(connection: Connection, sql: str) -> CursorResult[Any]:
@@ -48,9 +64,13 @@ def get_distinct_values_sql(table: str, column: str) -> str:
     ])
 
 
-def execute_sql_for_one_column_of_all(connection: Connection, sql: str) -> Iterator[str]:
+def execute_sql_for_one_column_of_one(connection: Connection, sql: str) -> Any:
+    return execute_sql(connection, sql).one()[0]
+
+
+def execute_sql_for_one_column_of_all(connection: Connection, sql: str) -> Iterator[Any]:
     return get_one_column(execute_sql(connection, sql).all())
 
 
-def get_distinct_values(connection: Connection, table: str, column: str) -> Iterator[str]:
+def get_distinct_values(connection: Connection, table: str, column: str) -> Iterator[Any]:
     return execute_sql_for_one_column_of_all(connection, get_distinct_values_sql(table, column))
