1cfe878ca3d4ed68a1718042ef85c579b939d116
Dmitry Bogdanov <bogdanov.d.a.1994@gmail.com>
2025-08-08 22:46:46 +0300

    Refactor edpu\clipboard_stack.py

--- edpu/clipboard_stack.py
+++ edpu/clipboard_stack.py
@@ -1,48 +1,69 @@
-def _main() -> None:
-    from . import tkinter_utils
-    import tkinter
+from typing import Callable
 
-    root = tkinter.Tk()
-    root.title('Clipboard stack')
 
-    stack: list[str] = []
-
-    def update_out_text() -> None:
-        out_text.delete(1.0, tkinter.END)
-        out_text.insert(tkinter.END, '\n----------\n'.join(stack))
-
-    def push() -> None:
-        from pyperclip import paste
-        stack.append(paste())
-        update_out_text()
-
-    def pop() -> None:
-        if len(stack) > 0:
-            from pyperclip import copy
-            copy(stack.pop())
-            update_out_text()
-
-    push_button = tkinter.Button(root, text='Push (F6)', command=push)
-    push_button.pack(side=tkinter.TOP, fill=tkinter.X)
-    root.bind('<F6>', lambda _: push())
-
-    pop_button = tkinter.Button(root, text='Pop (F7)', command=pop)
-    pop_button.pack(side=tkinter.TOP, fill=tkinter.X)
-    root.bind('<F7>', lambda _: pop())
-
-    out_text = tkinter.Text(root, height=40, width=120)
-    out_text_sb = tkinter.Scrollbar(root)
-
-    out_text.pack(side=tkinter.LEFT, fill=tkinter.Y)
-    out_text_sb.pack(side=tkinter.LEFT, fill=tkinter.Y)
-
-    out_text.config(yscrollcommand=out_text_sb.set)
-    out_text_sb.config(command=out_text.yview)
-
-    tkinter_utils.center_window(root)
-    tkinter.mainloop()
+VoidFn = Callable[[], None]
 
 
 def main() -> None:
-    from .tkinter_utils import handle_errors
-    handle_errors(_main)
+    from .tkinter_utils import autosize_window
+    from tkinter import Tk, NSEW
+    from tkinter.scrolledtext import ScrolledText
+
+    root = Tk()
+    root.title('clipboard_stack')
+    root.columnconfigure(0, weight=1)
+
+    def init_data() -> tuple[VoidFn, VoidFn]:
+        data: list[str] = []
+
+        def update() -> None:
+            from .tkinter_utils import text_widget_set
+            text_widget_set(text, f'\n{'-' * 64}\n'.join(data))
+
+        def push() -> None:
+            from pyperclip import paste
+            data.append(paste())
+            update()
+
+        def pop() -> None:
+            if len(data) > 0:
+                from pyperclip import copy
+                copy(data.pop())
+                update()
+
+        return (push, pop)
+
+    push, pop = init_data()
+
+    def add_buttons() -> None:
+        from tkinter.ttk import Frame
+
+        root.rowconfigure(0)
+        frame = Frame(root)
+        frame.grid(row=0, column=0, sticky=NSEW)
+        frame.rowconfigure(0, weight=1)
+
+        def add_button(name: str, key: str, command: VoidFn, column: int) -> None:
+            frame.columnconfigure(column, weight=1)
+
+            from tkinter.ttk import Button
+            Button(
+                frame,
+                text=f'{name} ({key})',
+                command=command
+            ).grid(row=0, column=column, sticky=NSEW)
+
+            from .string_utils import angle_brackets_wrap
+            root.bind(angle_brackets_wrap(key), lambda _: command())
+
+        add_button('Push', 'F6', push, 0)
+        add_button('Pop', 'F7', pop, 1)
+
+    add_buttons()
+
+    root.rowconfigure(1, weight=1)
+    text = ScrolledText(root)
+    text.grid(row=1, column=0, sticky=NSEW)
+
+    autosize_window(root)
+    root.mainloop()
